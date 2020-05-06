e9ade4be6fd71ad96ecdd7865e04729883bade94
romefar <vlad.romanovr@gmail.com>
2020-05-06 22:25:14 +0300

    updated db conn

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+require('dotenv/config')
 
-mongoose.connect(`mongodb://${process.env.DB_DEV_HOST}:${process.env.DB_DEV_PORT}/${process.env.DB_DEV_NAME}`, {
+mongoose.connect(`mongodb://${process.env.DB_DEV_HOST}:${process.env.DB_DEV_DB_PORT}/${process.env.DB_DEV_NAME}`, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: true
@@ -12,3 +12,5 @@ mongoose.connect(`mongodb://${process.env.DB_DEV_HOST}:${process.env.DB_DEV_PORT
   .catch(err => {
     console.log(err)
   })
+
+mongoose.set('useCreateIndex', true)
