d4b6129f849ae1472f88be552d0a04a106527248
romefar <vlad.romanovr@gmail.com>
2020-05-08 20:24:13 +0300

    updated cardHolderName regex

--- src/schemas/creditCardSchema.js
+++ src/schemas/creditCardSchema.js
@@ -4,13 +4,13 @@ const creditCardSchema = new Schema({
   cardNumber: {
     type: String,
     required: [true, 'Card number is required.'],
-    match: [/^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/g, 'Invalid card number.']
+    match: [/^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/, 'Invalid card number.']
   },
   cardHolder: {
     type: String,
     required: [true, 'Car holdrer value is required.'],
     uppercase: true,
-    match: [/^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/g, 'Card holder name can only contain the following characters: a-z, A-Z, \', hyphen and space.']
+    match: [/^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/, 'Card holder name can only contain the following characters: a-z, A-Z, \', hyphen and space.']
   },
   cardValidDate: {
     type: Date,
