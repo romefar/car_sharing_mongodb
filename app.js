c7e5de3b0580946fb56716e66c695fdbf54d79c9
romefar <vlad.romanovr@gmail.com>
2020-05-08 20:19:07 +0300

    updated entry point

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express')
+const chalk = require('chalk')
 require('./src/db/mongoose')
 require('dotenv/config')
+require('./src/initialData')
+
 const carsRoute = require('./src/routes/cars')
 
 const app = express()
+const port = process.env.DB_DEV_PORT || 3000
 app.use(express.json())
-app.use(carsRoute)
+app.use('/api/v1/', carsRoute)
 
-app.listen(process.env.DB_DEV_PORT, () => {
-  console.log(`The server is running on port ${process.env.DB_DEV_PORT}`)
+app.listen(port, () => {
+  console.log(chalk.green(`The server is running on port ${port}`))
 })
 
 //  C:/Users/Vladislav/mongodb/bin/mongod.exe --dbpath=C:/Users/Vladislav/mongodb-data
